8cb748b304a2511e7d38bae3ffc7ea9fe286c8ec
Manthan Patel <manthanpl93@live.com>
2020-08-23 23:16:46 +0530

    Mysql connection string fixed

--- api/config/knexfile.js
+++ api/config/knexfile.js
@@ -3,14 +3,14 @@
 module.exports = {
   development: {
     client: 'mysql',
-    connection: 'mysql://root:root@localhost:3306/CarousalBeverages',
+    connection: 'mysql://root:root@localhost:3306/databasename',
     migrations: {
       directory: '../migrations'
     }
   },
   production: {
     client: 'mysql',
-    connection: 'mysql://root:root@localhost:3306/CarousalBeverages',
+    connection: 'mysql://root:root@localhost:3306/databasename',
     migrations: {
       directory: '../migrations'
     }
